ce78bd8b2102a61f3424b3678ab1c86c9bb895ac
Temitope Olaitan Michael <temitopeolaitanmichael@gmail.com>
2025-11-25 18:02:10 +0100

    fix: Remove duplicate isDarkMode extension (velocity_x already provides it) and update version to 1.6.0+94

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -19,7 +19,7 @@ extension BuildContextExtended on BuildContext {
   Color get adaptive87 => theme.dividerColor.withOpacity(0.87);
   Color get adaptive => theme.dividerColor;
   Color get shadow => theme.brightness == Brightness.dark ? Colors.transparent : Colors.black12;
-  bool get isDarkMode => theme.brightness == Brightness.dark;
+  // isDarkMode is provided by velocity_x package, no need to redefine
   bool get isSmall => MediaQuery.of(this).size.height < 700;
   bool get isExtraSmall => MediaQuery.of(this).size.height < 600;
   bool get isTablet => MediaQuery.of(this).size.height > 1100;
